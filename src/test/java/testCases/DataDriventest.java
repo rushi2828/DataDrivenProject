be9ff103a4ff77f05f21ab0832ecb2085410dbc3
rushi2828 <rushi2828@gmail.com>
2018-11-28 17:45:11 +0530

    Added resource package

--- src/test/java/testCases/DataDriventest.java
+++ src/test/java/testCases/DataDriventest.java
@@ -2,7 +2,6 @@ package testCases;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -15,9 +14,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class DataDriventest {
